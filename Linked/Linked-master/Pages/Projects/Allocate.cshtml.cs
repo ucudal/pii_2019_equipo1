5d6dd96f633889b6173783d560558d11b6d5b65c
IgnacioSica <42221983+IgnacioSica@users.noreply.github.com>
2019-06-09 22:06:08 -0300

    Allocate finalizado

--- Linked/Linked-master/Pages/Projects/Allocate.cshtml.cs
+++ Linked/Linked-master/Pages/Projects/Allocate.cshtml.cs
@@ -9,34 +9,76 @@ using Linked.Models;
 using Microsoft.EntityFrameworkCore;
 using Linked.Areas.Identity.Data;
 
-namespace Linked.Pages.Projects
-{
-    public class AllocateModel : PageModel
-    {
+namespace Linked.Pages.Projects{
+    public class AllocateModel : PageModel{
         private readonly Linked.Models.LinkedContext _context;
 
-        public AllocateModel(Linked.Models.LinkedContext context)
-        {
+        public AllocateModel(Linked.Models.LinkedContext context){
             _context = context;
         }
 
         public Project Project { get; set; }
 
+        /*
         public IList<Technician> Technician {get;set;}
 
-        public async Task OnGetAsync(int? id)
-        {
+        public async Task OnGetAsync(int? id){
             Project = await _context.Project.FindAsync(id);
+            TechniciansAlternativos = LoadTechniciansAlternativos();
 
             var technicians = from t in _context.Technician
                  select t;
-            if (Project != null)
-            {
+
+            if (Project != null){
                 technicians = technicians.Where(tec => tec.Role.Equals(Project.Role)&&tec.Level.Equals(Project.Level));
             }
+
             Technician = await technicians.ToListAsync();
         }
+        */
+        public async Task OnGetAsync(int? id){
+            Project = await _context.Project.FindAsync(id);
+            TechniciansAlternativos = LoadTechniciansAlternativos();
+            Technician = LoadTechnicians();
+        }
         public Technician TechnicianSelected {get; set;}
+        public IEnumerable<Technician> Technician {get;set;}
+
+        public IEnumerable<Technician> TechniciansAlternativos {get;set;}
+
+        public IEnumerable<Technician> LoadTechniciansAlternativos(){
+            var db = _context;
+            IEnumerable<Technician> e = Enumerable.Empty<Technician>();
+            IEnumerable<Technician> f = Enumerable.Empty<Technician>();
+            try {
+                foreach(Employ emp in db.Employ.Where(p=> p.ProjectID == Project.ProjectID)){
+                    f = f.Concat(db.Technician.Where(t => t.TechnicianID == emp.TechnicianID).AsEnumerable());
+                }
+                foreach(Technician technician in db.Technician.Include(c=>c.Employers).AsEnumerable()){
+                    if (technician.Role == Project.Role && technician.Level != Project.Level && !f.Contains(technician)){
+                        e = e.Append(technician);
+                    }
+                }
+            }catch{}
+            return e;
+        }
+
+        public IEnumerable<Technician> LoadTechnicians(){
+            var db = _context;
+            IEnumerable<Technician> e = Enumerable.Empty<Technician>();
+            IEnumerable<Technician> f = Enumerable.Empty<Technician>();
+            try {
+                foreach(Employ emp in db.Employ.Where(p=> p.ProjectID == Project.ProjectID)){
+                    f = f.Concat(db.Technician.Where(t => t.TechnicianID == emp.TechnicianID).AsEnumerable());
+                }
+                foreach(Technician technician in db.Technician.Include(c=>c.Employers).AsEnumerable()){
+                    if (technician.Role == Project.Role && technician.Level == Project.Level && !f.Contains(technician)){
+                        e = e.Append(technician);
+                    }
+                }
+            }catch{}
+            return e;
+        }
 
         public async Task<IActionResult> OnPostAsync(int? id)
         {
@@ -55,51 +97,10 @@ namespace Linked.Pages.Projects
                 Employ NewEmploy = new Employ();
                 NewEmploy.TechnicianID = ParsedTechId;
                 NewEmploy.ProjectID = Project.ProjectID;
-                // NewEmploy.Technician = TechnicianSelected;
-                // NewEmploy.Project = Project;
                 _context.Employ.Add(NewEmploy);
                 await _context.SaveChangesAsync();
             }
             return RedirectToPage("./Index");
         }
     }
-}
-//         public IEnumerable<Technician> TechniciansAlternativos {get;set;}
-
-//         public IEnumerable<Technician> LoadTechniciansAlternativos(){
-//             var db = _context;
-//             IEnumerable<Technician> e = Enumerable.Empty<Technician>();
-//             IEnumerable<Technician> f = Enumerable.Empty<Technician>();
-//             try {
-//                 foreach(Employ emp in db.Employ.Where(p=> p.ProjectID == Project.ProjectID)){
-//                     f = f.Concat(db.Technician.Where(t => t.TechnicianID == emp.TechnicianID).AsEnumerable());
-//                 }
-//                 foreach(Technician technician in db.Technician.Include(c=>c.Employers).AsEnumerable()){
-//                     if (technician.Role == Project.Role && technician.Level != Project.Level && !f.Contains(technician)){
-//                         e = e.Append(technician);
-//                     }
-//                 }
-//             }catch{}
-//             return e;
-//         }
-
-//         public async Task<IActionResult> OnGetAsync(int? id)
-//         {
-//             if (id == null)
-//             {
-//                 return NotFound();
-//             }
-
-//             Project = await _context.Project.FirstOrDefaultAsync(m => m.ProjectID == id);
-
-//             Technicians = LoadTechnicians();
-//             TechniciansAlternativos = LoadTechniciansAlternativos();
-
-//             if (Project == null)
-//             {
-//                 return NotFound();
-//             }
-//             return Page();
-//         }
-//     }
-// }
+}       
