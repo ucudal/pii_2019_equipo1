675a0a7a5e0109d53382576416ae5d5d8b8ace12
IgnacioSica <42221983+IgnacioSica@users.noreply.github.com>
2019-06-22 22:28:27 -0300

    Changes in the model of the application

--- Linked/Linked-master/Model/Project.cs
+++ Linked/Linked-master/Model/Project.cs
@@ -1,7 +1,4 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Linq;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
@@ -11,35 +8,34 @@ namespace Linked.Models{
     public class Project{
         public string ProjectID { get; set; }
 
-        private string title;
-
-        [StringLength(60, MinimumLength = 3)]
-        [Required]
-        public string Title { get{return title;} 
-        set{if (!string.IsNullOrEmpty(value))
-        {title=value;}
-        } 
-        }
-
-        private string description;
-
-        [StringLength(60, MinimumLength = 3)]
-        [Required]
-        public string Description { get{return description;} 
-        set{if (!string.IsNullOrEmpty(value))
-        {description=value;}
-        } 
-        }
-        public bool CompletionStatus {get; set;}
         
+        [Required, StringLength(50, MinimumLength = 3), RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        public string Title { get; set; }
+
+
+        [Required, StringLength(150, MinimumLength = 3), RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        public string Description { get; set; }
+        
+
+        public bool CompletionStatus {get; set;}
+
+        
+        [Display(Name = "Date"), DataType(DataType.Date)]
         public DateTime Date { get; set; }
 
+
+        [Required]
         public Specialty Specialty{ get; set; }
-        //public Specialty Specialty;
+
+
+        [Required]
         public Level Level{ get; set; }
-        //public Level Level;
+
         
         public IList<Employ> Employees { get; set; } // Technicians
+        public IList<Interest> InterestedTechnicians { get; set; } // Interested Technicians
+        public IList<Requirement> Requirements { get; set; } // Required Technicians
+
 
         public string ClientID { get; set; } 
         public Client Client { get; set; }
