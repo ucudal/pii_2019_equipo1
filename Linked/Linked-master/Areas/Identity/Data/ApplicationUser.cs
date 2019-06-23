c2388fe12665a07de08aae56aa0af52cda91dff7
MartinAya <39680899+MartinAya@users.noreply.github.com>
2019-06-23 00:51:56 -0300

    PaginasTechnicianClient

--- Linked/Linked-master/Areas/Identity/Data/ApplicationUser.cs
+++ Linked/Linked-master/Areas/Identity/Data/ApplicationUser.cs
@@ -14,11 +14,6 @@ namespace Linked.Areas.Identity.Data
         [PersonalData]
         public string Name { get; set; }
 
-        [Display(Name = "Date of Birth")]
-        [DataType(DataType.Date)]
-        [PersonalData]
-        public DateTime DOB { get; set; }
-
         // Es necesario tener acceso a IdentityManager para poder buscar el rol de este usuario; esta propiedad se asigna cada vez que se
         // cambia el rol usando IdentityManager para poder buscar por rol después cuando no hay acceso a IdentityManager. La propiedad
         // puede ser null para los usuarios que todavía no tienen un rol asignado.
@@ -26,6 +21,8 @@ namespace Linked.Areas.Identity.Data
 
         // El IdentityManager que se recibe como argumento no se usa en el método; sólo fuera a que el rol del usuario sea cambiado cuando
         // existe en el contexto una instancia válida de IdentityManager.
+
+        public DateTime DOB { get; set; }
         public void AssignRole(UserManager<ApplicationUser> identityManager, string role)
         {
             if (identityManager == null)
